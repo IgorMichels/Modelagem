6f7b920eba9694835e657047728c88254a3a0a8f
Igor Michels <igorxadrezbr@gmail.com>
2020-12-16 10:39:11 -0300

    some changes

--- data_functions.py
+++ data_functions.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def catch_players(file, top):
-    # return a list with top players' ID
+    # returns a list with top players' ID
     players = pd.read_csv(file, header = None, names = ['date', 'rank', 'id', 'points'])
 
     top_players = []
@@ -13,11 +13,12 @@ def catch_players(file, top):
     return top_players
 
 def catch_games(file, top_players, surface, sets):
-    # return a dataframe with games of one year of top players in one surface
+    # returns a dataframe with games of one year of top players in one surface
     games = pd.read_csv(file)
     
     col = ['surface', 'tourney_date', 'winner_id', 'loser_id', 'score', 'best_of',
-           'winner_rank', 'winner_rank_points', 'loser_rank', 'loser_rank_points']
+           'winner_rank', 'winner_rank_points', 'loser_rank', 'loser_rank_points',
+           'tourney_level']
 
     for column in games:
         if column not in col:
@@ -36,10 +37,11 @@ def catch_games(file, top_players, surface, sets):
     return games
 
 def catch_all_games(files, rank, top, surface = 'Hard', sets = 3):
-    # return a dataframe with games of top players in one surface (default: hard)
+    # returns a dataframe with games of top players in one surface (default: hard)
     players = catch_players(rank, top)
     games = pd.DataFrame(columns = ['surface',
                                     'tourney_date',
+                                    'tourney_level',
                                      'winner_id',
                                      'loser_id',
                                      'score',
@@ -51,13 +53,14 @@ def catch_all_games(files, rank, top, surface = 'Hard', sets = 3):
 
     for file in files:
         data = catch_games(file, players, surface, sets)
-        games = pd.concat([games, data])
+        games = pd.concat([games, data], sort = False)
 
     games.reset_index(inplace = True)
     games.pop('index')
     games.pop('surface')
     games.pop('best_of')
     games = games.reindex(columns = ['tourney_date',
+                                     'tourney_level',
                                      'score',
                                      'winner_id',
                                      'winner_rank',
